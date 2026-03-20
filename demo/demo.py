32aa4d43864db29c5568719474b5be54196db3c1
Thuần Trần <thuantran2704@gmail.com>
2026-03-20 09:47:01 -0400

    script for dijkstra test

--- demo/demo.py
+++ demo/demo.py
@@ -6,8 +6,9 @@ from gem5.components.cachehierarchies.classic.private_l1_private_l2_cache_hierar
 from gem5.components.memory.single_channel import SingleChannelDDR4_2400
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.isas import ISA
-from gem5.resources.resource import obtain_resource
+# from gem5.resources.resource import obtain_resource
 from gem5.simulate.simulator import Simulator
+from gem5.resources.resource import BinaryResource
 
 
 cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
@@ -15,8 +16,8 @@ cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
 )
 
 memory = SingleChannelDDR4_2400()
-processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM, num_cores=1)
-# processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=1)
+# processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM, num_cores=1)
+processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=1)
 
 board = SimpleBoard(
     clk_freq="3GHz",
@@ -25,12 +26,24 @@ board = SimpleBoard(
     cache_hierarchy=cache_hierarchy,
 )
 
-gabps = obtain_resource("arm-gapbs-bfs-run")
-board.set_workload(gabps)
+# gabps = obtain_resource("x86-gapbs-bfs-run")
+# board.set_workload(gabps)
 
 # gabps = obtain_resource("x86-gapbs-bfs-run")
 # gabps.set_parameter("env_list", "OMP_NUM_THREADS=1")
-# board.set_workload(gabps);
+# board.set_workload(gabps)
+
+from gem5.resources.resource import BinaryResource
+
+binary = BinaryResource(
+    local_path="/mnt/c/src/eel/dijkstra"
+)
+
+board.set_workload(
+    binary,
+    arguments=["input.txt"],
+    working_directory="/mnt/c/src/eel"
+)
 
 simulator = Simulator(board=board)
 simulator.run()
